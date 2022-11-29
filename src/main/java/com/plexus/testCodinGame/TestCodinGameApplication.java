cbd8d9debe90cfde758b182e98351a71427178db
Santiago Suarez <santiago.suarezcd@gmail.com>
2022-11-29 16:59:49 -0300

    Add Cache in API getHeroesById /heroes/{id}

--- src/main/java/com/plexus/testCodinGame/TestCodinGameApplication.java
+++ src/main/java/com/plexus/testCodinGame/TestCodinGameApplication.java
@@ -2,7 +2,9 @@ package com.plexus.testCodinGame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class TestCodinGameApplication {
 
